751956b13469f2e8cd1dc5d83339b303712da559
Thermiprettie <tadekolasarah@yahoo.com>
2023-05-10 13:48:37 -0700

    tasks

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # Using strace, find out why Apache is returning a 500 error,
-# fix it and then automate it using Puppet
+# fix it then automate it using Puppet 
 
-exec {'fix-wordpress':
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
